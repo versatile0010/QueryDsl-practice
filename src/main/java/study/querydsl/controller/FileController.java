0afb723201b82ba48e2eec454047572605643220
versatile0010 <siam0294@naver.com>
2023-10-07 01:16:54 +0900

    docs: swagger set up

--- src/main/java/study/querydsl/controller/FileController.java
+++ src/main/java/study/querydsl/controller/FileController.java
@@ -1,5 +1,7 @@
 package study.querydsl.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import study.querydsl.service.StorageService;
 
 import java.io.IOException;
 
+@Tag(name = "이미지 파일 업로드 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +21,18 @@ import java.io.IOException;
 public class FileController {
     private final StorageService storageService;
     @PostMapping
+    @Operation(summary = "단일 이미지 업로드", description =
+                    """
+                    [RequestParameter] 타입: MultipartFile, 이름: image
+                    
+                    [Response] upload file 이름  
+                    """)
     public ResponseEntity<?> uploadImageToFileSystem(@RequestParam("image") MultipartFile file) throws IOException {
         log.info("[file 을 upload 합니다.]");
         String uploadImage = storageService.uploadImageToFileSystem(file);
         return ResponseEntity.status(HttpStatus.OK)
                 .body(uploadImage);
     }
-
     @GetMapping("/{fileName}")
     public ResponseEntity<?> downloadImageToFileSystem(@PathVariable("fileName") String fileName) throws IOException {
         log.info("[file 을 download 합니다.]");
